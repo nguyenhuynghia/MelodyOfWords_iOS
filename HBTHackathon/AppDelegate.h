39e22807403aa18bb478ff214a05671c9d193402
NghiaNH <nghianh@hblab.vn>
2017-12-16 11:10:36 +0700

    Initial Commit

---
+++ HBTHackathon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HBTHackathon
+//
+//  Created by NghiaNH on 12/16/17.
+//  Copyright © 2017 NghiaNH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HBTHackathon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HBTHackathon
+//
+//  Created by NghiaNH on 12/16/17.
+//  Copyright © 2017 NghiaNH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
